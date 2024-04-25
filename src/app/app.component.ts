6f10c4617063fc46ccacf2925be5b1e46846d39a
NataliaMK2 <84319418+NataliaMK2@users.noreply.github.com>
2024-04-25 16:13:51 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaProductosComponent } from "./lista-productos/lista-productos.component";
+import { AltaProductosComponent } from "./alta-baja.productos/alta-productos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ListaProductosComponent, AltaProductosComponent]
 })
 export class AppComponent {
   title = 'Abril25';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { AltaProductosComponent } from './alta-baja.productos/alta-productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'productos', component: ListaProductosComponent },
+  { path: 'alta', component: AltaProductosComponent },
+  { path: '', redirectTo: '/productos', pathMatch: 'full' }
+];
