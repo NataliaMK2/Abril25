69bfc2ed85e6673c07f368bdaedc1f957deebc77
NataliaMK2 <84319418+NataliaMK2@users.noreply.github.com>
2024-04-25 16:19:07 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaProductosComponent } from './lista-productos/lista-productos.component';
 import { AltaProductosComponent } from './alta-baja.productos/alta-productos.component';
-
+//en alta tambien está eliminar
 export const routes: Routes = [
   { path: 'productos', component: ListaProductosComponent },
   { path: 'alta', component: AltaProductosComponent },
